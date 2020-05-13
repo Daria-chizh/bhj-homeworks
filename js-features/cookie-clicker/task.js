11d15b3877ff2fe142f43589a109f3637754c218
Daria Ablovatskaia <daryana_chizhova@list.ru>
2020-05-13 22:09:17 +0700

    implement advanced level of cookie-clicker task

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,15 +1,30 @@
 const img = document.getElementById("cookie");
 const counter = document.getElementById("clicker__counter");
+const clickSpeedElement = document.getElementById("clicker__speed");
+
+let firstClickTime;
 
 img.onclick = function() {
-  const amount = Number(counter.textContent);
-  counter.textContent = amount + 1;
-
   // resize image
   const currentWidth = img.width;
   if (currentWidth === 200) {
     img.width = 300;
   } else if (currentWidth === 300) {
     img.width = 200;
+  }
+
+  // update click count
+  const amount = Number(counter.textContent) + 1;
+  counter.textContent = amount;
+
+  // update click speed
+  if (firstClickTime) {
+    const currentTime = new Date();
+    const durationInMS = Number(currentTime) - Number(firstClickTime);
+    const durationInSec = durationInMS / 1000;
+    const clickSpeed = durationInSec / amount;
+    clickSpeedElement.textContent = clickSpeed.toFixed(2);
+  } else {
+    firstClickTime = new Date();
   }
 };
