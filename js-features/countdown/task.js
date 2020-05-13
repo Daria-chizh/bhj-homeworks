453e54e884326c1eb1623958e35948438cb14ab5
Daria Ablovatskaia <daryana_chizhova@list.ru>
2020-05-13 22:08:08 +0700

    implement advanced level of count down task

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -15,3 +15,34 @@ intervalID = setInterval(updateCountdown, 1000);
 
 // Задание 1. Повышенный уровень сложности 1
 
+const longTimerElement = document.getElementById("longTimer");
+
+let longTimerIntervalId = 0;
+
+const updateLongTimer = function() {
+  // format "HH:MM:SS"
+  const timerValue = longTimerElement.textContent;
+  const timeParts = timerValue.split(':');
+  let hour = timeParts[0];
+  let minute = timeParts[1];
+  let second = timeParts[2];
+
+  second--;
+  if (second < 0) {
+    second = 59;
+    minute--;
+    if (minute < 0) {
+      minute = 59;
+      hour--;
+      if (hour < 0) {
+        window.alert("Вы победили в конкурсе!");
+        clearInterval(longTimerIntervalId);
+        return;
+      }
+    }
+  }
+
+  longTimerElement.textContent = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}`;
+};
+
+longTimerIntervalId = setInterval(updateLongTimer, 1000);
