db77f2f5833a3cf5dfa640045a2208f9f489a9e7
Daria Ablovatskaia <daryana_chizhova@list.ru>
2020-05-14 21:46:44 +0700

    implement advanced level of menu task

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,9 +5,17 @@ for (const menuLinkElement of menuLinkElements) {
     const menuItemElement = menuLinkElement.closest(".menu__item");
     const subMenuElement = menuItemElement.querySelector(".menu");
     if (subMenuElement) {
-      subMenuElement.classList.toggle("menu_active");
+      if (subMenuElement.classList.contains("menu_active")) {
+        subMenuElement.classList.remove("menu_active");
+      } else {
+        const mainMenuElement = subMenuElement.closest('.menu_main');
+        const currentOpenSubMenuElement = mainMenuElement.querySelector('.menu_active');
+        if (currentOpenSubMenuElement) {
+          currentOpenSubMenuElement.classList.remove("menu_active");
+        }
+        subMenuElement.classList.add("menu_active");
+      }
       return false;
     }
   };
 }
-
